62d460fc3c943ae898acdc3c034bdf5b3209c13a
NoSignificantHarassment <16279008+unfounded-worries_0@user.noreply.gitee.com>
2026-08-18 23:57:18 +0800

    暂存

--- Robot/infantry1/Chassis/fsm/pyro_wl_manual_state.cpp
+++ Robot/infantry1/Chassis/fsm/pyro_wl_manual_state.cpp
@@ -62,9 +62,6 @@ void wl_chassis_t::fsm_active_t::state_manual_t::execute(wl_chassis_t *owner)
                             -PI, PI);
 
 
-
-    debug1 = owner->_ctx.data.leg[leg_def::R].target_leg_rad;
-    debug2 = owner->_ctx.data.leg[leg_def::R].current_leg_rad;
     owner->_manual_control();
     owner->_vmc_trans_v2j();
     owner->_send_joint_torque();
