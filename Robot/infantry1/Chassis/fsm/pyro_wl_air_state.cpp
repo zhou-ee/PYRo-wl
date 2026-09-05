5b83bea8c97e651b1b382a62c01258514fd46f2c
ssy <16392174+ssyssyssy2007@user.noreply.gitee.com>
2026-09-06 00:37:28 +0800

    虚拟墙加大，支持力检测

--- Robot/infantry1/Chassis/fsm/pyro_wl_air_state.cpp
+++ Robot/infantry1/Chassis/fsm/pyro_wl_air_state.cpp
@@ -58,7 +58,7 @@ void wl_chassis_t::_calc_support_force()
         // Motor torque feedback excludes the passive gas spring. Add the
         // physical spring contribution back for contact/takeoff detection.
         const float support_force_raw =
-            (leg.current_F_L + leg.gas_spring_force) * cos_beta +
+            leg.current_F_L  * cos_beta -
             leg.current_T_p / length * sin_beta +
             SUPPORT_FORCE_EFFECTIVE_MASS * leg_endpoint_accel;
         _ctx.data.airborne.support_force[i] +=
